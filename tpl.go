058b0d03739f76aa1fd151e12ae3f5d897a5e7c0
liliqiang <liliqiang@linuxdeepin.com>
2016-02-24 11:02:52 +0800

    update header of generated file.

--- tpl.go
+++ tpl.go
@@ -1,7 +1,7 @@
 package main
 
 const Prefix = `// Package {{ PkgName }} provides dbus interface for settings.
-// This file is autogen by {{ ToolVersion }}, not modified it.
+// This file is autogen by gschema-to-godbus {{ ToolVersion }}, not modified it.
 package {{ PkgName }}
 
 `
