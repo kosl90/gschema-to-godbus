23f74f759d41a917e1f8b3e4faf01f8e7138a10b
liliqiang <liliqiang@linuxdeepin.com>
2016-02-24 10:30:38 +0800

    improve help message.

--- cmd.go
+++ cmd.go
@@ -6,10 +6,10 @@ import (
 
 var (
 	_schema    *string = kingpin.Flag("schema", "schema which is going to be translated.").Required().Short('s').String()
-	_pkgName   *string = kingpin.Flag("pkg_name", "package name used in settings.").Required().Short('n').String()
-	_dbusName  *string = kingpin.Flag("dest", "dbus dest used for settings.").Required().Short('d').String()
-	_dbusPath  *string = kingpin.Flag("path", "dbus path used for settings.").Required().Short('p').String()
-	_outputDir *string = kingpin.Flag("output_dir", "output directory to save generated files.").Default(".").Short('o').ExistingDir()
+	_pkgName   *string = kingpin.Flag("pkg_name", "package name used for golang package, default is main.").Default("main").Short('n').String()
+	_dbusName  *string = kingpin.Flag("dest", "dbus dest used for dbus.").Required().Short('d').String()
+	_dbusPath  *string = kingpin.Flag("path", "dbus path used for dbus.").Required().Short('p').String()
+	_outputDir *string = kingpin.Flag("output_dir", "output directory to save generated files, default is current directory.").Default(".").Short('o').ExistingDir()
 )
 
 func parseCMD() {
